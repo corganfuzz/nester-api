9d08fb28b938ccb412e24e1041307ba0e62eddea
corganfuzz <gruizcavero@uh.edu>
2019-06-02 11:55:35 -0500

    start removing fake data

--- src/items/items.module.ts
+++ src/items/items.module.ts
@@ -1,9 +1,13 @@
+import { MongooseModule } from '@nestjs/mongoose';
+import { ItemSchema } from './schemas/item.schema';
 import { Module } from '@nestjs/common';
 import { ItemsController } from './items.controller';
 import { ItemsService } from './items.service';
 
 @Module({
-  imports: [],
+  imports: [MongooseModule.forFeature([{
+    name: 'Item', schema: ItemSchema,
+  }])],
   controllers: [ItemsController],
   providers: [ItemsService],
 })
